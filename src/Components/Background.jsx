066bedbb19fc9cdacd1eaf5e0174d1ea0f718a49
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-09-16 01:23:53 +0500

    first commit

---
+++ src/Components/Background.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../css/Background.css"
+export default function Background() {
+    return (
+        <div class="lines">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+    )
+}
